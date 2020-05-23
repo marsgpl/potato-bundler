83d4b2ef816e410c22711ad4a1c31afc45938768
marsgpl <marsgpl@gmail.com>
2020-05-23 19:35:57 +0300

    initing with args

--- src/PotatoBundler.js
+++ src/PotatoBundler.js
@@ -1,6 +1,25 @@
 class PotatoBundler {
-    constructor() {
-        console.log('Potato Bundler');
+    constructor(args) {
+        this.parseArgs(args);
+        console.log('this:', this);
+    }
+
+    parseArgs(args) {
+        for (let i = 2; i < args.length; ++i) {
+            const arg = args[i];
+
+            if (arg.startsWith('--') && arg.includes('=')) {
+                const [name, value] = arg.substr(2).split('=');
+
+                if (name === 'in' || name === 'src' || name === 'from') {
+                    this.src = value;
+                } else if (name === 'out' || name === 'dst' || name === 'to') {
+                    this.dst = value;
+                } else if (name == 'entry') {
+                    this.entry = value;
+                }
+            }
+        }
     }
 }
 
